6771854521c72a0268e720484a20c4583983c1cf
Matt C <me@mitt.dev>
2022-12-11 09:59:24 +0000

    day 10 rename

--- 10/test_day10.py
+++ 10/test_day10.py
@@ -1,6 +1,6 @@
 import day10
 
 def test_part1():
-    with open("10/10_p1_test.txt") as f:
+    with open("10/test_day10_data.txt") as f:
         data = f.read().splitlines()
         assert day10.day10(data) == 13140
